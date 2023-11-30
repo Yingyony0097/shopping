4f83077f651bbfcf698619420852a649326edecb
saimond <yingyong0097@gmail.com>
2023-11-30 12:46:20 +0700

    add more bug2

--- productsapp/models.py
+++ productsapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name=models.CharField(max_length=50)
     description=models.TextField()
-    price=models.DecimalField(max_digits=10,decimal_places=2)
+    price=models.DecimalField(max_digits=20,decimal_places=2)
     stock=models.IntegerField()
     isTrending=models.BooleanField(default=False)
     image=models.ImageField(upload_to="products",blank=True)
